04e6da507214d54c8c681653cf4d29f2c005db39
nashehannafii <nashehannafii@gmail.com>
2022-09-07 03:27:03 +0700

    first commit

---
+++ controllers/DashboardController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\controllers;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+class DashboardController extends \yii\web\Controller
+{
+    public function behaviors()
+    {
+        $this->layout = 'cooladmin/main';
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
